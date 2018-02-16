74f64c98872e51f8425a8d9097092fb93a6706dd
fullnoize <fullnoize@localhost>
2018-02-17 01:33:47 +1100

    Release candidate 1

--- au.blindmot.mtmlabels/src/au/blindmot/mtmlabels/labelfactory/BLDMtomLabelFactory.java
+++ au.blindmot.mtmlabels/src/au/blindmot/mtmlabels/labelfactory/BLDMtomLabelFactory.java
@@ -2,14 +2,15 @@ package au.blindmot.mtmlabels.labelfactory;
 
 import org.adempiere.base.IProcessFactory;
 import org.compiere.process.ProcessCall;
-import au.blindmot.mtmlabels.MtmLabels;
+
+import au.blindmot.mtmlabels.processes.MtmLabels;
 
 
 public class BLDMtomLabelFactory implements IProcessFactory {
 
 	@Override
 	public ProcessCall newProcessInstance(String className) {
-		if(className.equals("au.blindmot.mtmlabels.MtmLabels"))return new MtmLabels();
+		if(className.equals("au.blindmot.mtmlabels.processes.MtmLabels"))return new MtmLabels();
 		return null;
 	}
 
